7f7198af3bd2a3d8afac86fc0e2096a220e54190
Sarthak <mehtasarthak2@gmail.com>
2024-07-25 12:14:20 +0530

    Blogpage url to actual url

--- src/components/Blogpost.jsx
+++ src/components/Blogpost.jsx
@@ -14,7 +14,7 @@ export function Blogpost({ bloglist }) {
   const url = process.env.NODE_ENV === 'production' ? "https://sharpmoney-backend.onrender.com" : "http://localhost:3002";
 
   const santizedcontent = DOMPurify.sanitize(blog.content);
-  const postUrl = `https://sharpmoney-blog-frontend.vercel.app/blog/${blog.slug}`; // Update this to your actual front-end URL
+  const postUrl = `https://www.sharpmoney.co.in//blog/${blog.slug}`; // Update this to your actual front-end URL
 
   const linkedinShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(postUrl)}`;
   const whatsappShareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(postUrl)}`;
@@ -39,7 +39,7 @@ const iconStyle = {
     <div>
     <Navbar2/>
     <div className='blogpost-container'>
-    {blog.imageUrl && <img src={url+`${blog.imageUrl}`} alt={blog.title} style={imageStyle}/>}<br></br>
+    {blog.imageUrl && <img src={blog.imageUrl} alt={blog.title} style={imageStyle}/>}<br></br>
     <div className="share-icons">
           <a href={linkedinShareUrl} target="_blank" rel="noopener noreferrer">
             <FaLinkedin style={iconStyle} />
